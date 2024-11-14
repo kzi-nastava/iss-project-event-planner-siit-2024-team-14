bf694fe858ebfedab19b8cf3492bffbc07f4622b
Ћетковић Алекса <cetkovic.sv77.2022@uns.ac.rs>
2024-11-14 12:39:07 +0100

    Initialize project using https://start.spring.io

---
+++ src/main/java/edu/ftn/iss/eventplanner/EventPlannerApplication.java
@@ -0,0 +1,11 @@
+package edu.ftn.iss.eventplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlannerApplication {
+
+	public static void main(String[] args) { SpringApplication.run(EventPlannerApplication.class, args); }
+
+}
