d047bf611bfc38ca0f6e99ee007161696f4b6e73
Tea <belicc.tt@gmail.com>
2024-11-24 22:42:46 +0100

    Pushing Model Data

---
+++ src/main/java/edu/ftn/iss/eventplanner/model/Admin.java
@@ -0,0 +1,11 @@
+package edu.ftn.iss.eventplanner.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Admin extends User {
+}
