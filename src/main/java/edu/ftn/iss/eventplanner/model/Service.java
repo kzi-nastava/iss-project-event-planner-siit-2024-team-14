32d1d019ed1c24ac78c23b3fa45c408f1d8e4bea
milicabosancic1 <milicabosancic1612@gmail.com>
2024-11-28 02:16:29 +0100

    added filter and serch for solutions

--- src/main/java/edu/ftn/iss/eventplanner/model/Service.java
+++ src/main/java/edu/ftn/iss/eventplanner/model/Service.java
@@ -2,29 +2,28 @@ package edu.ftn.iss.eventplanner.model;
 
 import lombok.*;
 import jakarta.persistence.*;
+
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Service {
+@DiscriminatorValue("SERVICE")
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Service extends Solution {
 
-    private String name;
-    private String description;
-    private double price;
-    private double discount;
-    private String imageUrl;
-    private int duration;
-    private String reservationType;
-    private LocalDate reservationDate;
-    private LocalDate cancellationDate;
+    private int duration; // Trajanje usluge (u minutima)
+    private String reservationType; // Tip rezervacije (npr. online, telefonski)
+    private LocalDate reservationDate; // Datum kada je usluga dostupna
+    private LocalDate cancellationDate; // Datum kada usluga više nije dostupna
 
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
+    public Service(Long id, String name, String description, double price, double discount, String imageUrl, boolean isAvailable, int duration, String reservationType, LocalDate reservationDate, LocalDate cancellationDate, List<Comment> comments) {
+        super(id, name, description, price, discount, imageUrl, isAvailable, true, false, comments);
+        this.duration = duration;
+        this.reservationType = reservationType;
+        this.reservationDate = reservationDate;
+        this.cancellationDate = cancellationDate;
+    }
 }
