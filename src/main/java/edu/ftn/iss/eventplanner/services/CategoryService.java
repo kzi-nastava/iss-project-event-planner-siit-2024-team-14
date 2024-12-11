35fc2b291227cc5236d1ce8662595621f40090cb
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2024-12-11 19:05:14 +0100

    Add exception handling to category service

--- src/main/java/edu/ftn/iss/eventplanner/services/CategoryService.java
+++ src/main/java/edu/ftn/iss/eventplanner/services/CategoryService.java
@@ -1,16 +1,20 @@
 package edu.ftn.iss.eventplanner.services;
 
 import edu.ftn.iss.eventplanner.entities.SolutionCategory;
+import edu.ftn.iss.eventplanner.events.CategoryNameChangedEvent;
+import edu.ftn.iss.eventplanner.exceptions.BadRequestException;
+import edu.ftn.iss.eventplanner.exceptions.InternalServerError;
+import edu.ftn.iss.eventplanner.exceptions.NotFoundException;
 import edu.ftn.iss.eventplanner.repositories.CategoryRepository;
-import jakarta.annotation.Nullable;
+import jakarta.validation.ConstraintViolationException;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -32,61 +36,99 @@ public class CategoryService {
 
 
     public SolutionCategory getCategoryById(int id) {
-        return categories.findById(id)
-                // TODO: Make custom exceptions
-                .orElseThrow(() -> new IllegalArgumentException("Category not found"));
+        Optional<SolutionCategory> category;
+        try {
+            category = categories.findById(id);
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
+
+        return category.orElseThrow(() -> new NotFoundException("Category not found!"));
     }
 
 
     public Optional<SolutionCategory> findCategoryById(int id) {
-        return categories.findById(id);
+        try {
+            return categories.findById(id);
+        } catch  (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
     }
 
 
-    public Optional<SolutionCategory> findCategoryByName(String name) { return categories.findByName(name); }
+    public Optional<SolutionCategory> findCategoryByName(String name) {
+        try {
+            return categories.findByName(name);
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
+    }
 
 
 
     public Collection<SolutionCategory> getAllCategories() {
-        return categories.findAll();
+        try {
+            return categories.findAll();
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
     }
 
 
 
-    public SolutionCategory insertCategory(SolutionCategory category) {
-        category.setId(null);
-        return categories.save(category);
+    public SolutionCategory insertCategory(SolutionCategory categoryRequest) {
+        categoryRequest.setId(null); // probably should create a new category
+
+        try {
+            return categories.save(categoryRequest);
+        } catch (DataIntegrityViolationException | ConstraintViolationException | IllegalArgumentException ex) {
+            throw new BadRequestException();
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
     }
 
 
+    @Transactional
+    public SolutionCategory updateCategory(SolutionCategory categoryRequest) {
+        SolutionCategory category = getCategoryById(categoryRequest.getId());
+        String oldName = category.getName();
 
-    public SolutionCategory updateCategory(SolutionCategory category) {
-        SolutionCategory oldCategoryState = getCategoryById(category.getId());
-        boolean hasNameChanged = !oldCategoryState.getName().equals(category.getName());
-        if (hasNameChanged) onCategoryNameChanged(category);
-        category = categories.save(category);
-        return category;
+        SolutionCategory updatedCategory;
+        try {
+            updatedCategory = categories.save(categoryRequest);
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
+
+        // TODO: Maybe add domain events instead
+        boolean hasNameChanged = !oldName.equals(updatedCategory.getName());
+        if (hasNameChanged)
+            eventPublisher.publishEvent(new CategoryNameChangedEvent(category.getId(), oldName, updatedCategory.getName()));
+
+        return updatedCategory;
     }
 
 
 
     public void deleteCategoryById(int id) {
-        SolutionCategory category = getCategoryById(id);
-        // TODO: Prevent deletion if services/products that are categorized into this category exist
-        categories.deleteById(id);
+        try {
+            SolutionCategory category = getCategoryById(id);
+            // TODO: Prevent deletion if services/products that are categorized into this category exist
+            categories.deleteById(id);
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
     }
 
 
 
     public void deleteAllCategories() {
-        // TODO: Prevent deletion ...
-        categories.deleteAll();
-    }
-
-
-
-    protected void onCategoryNameChanged(SolutionCategory category) {
-        // TODO
-        eventPublisher.publishEvent(/* new CategoryNameChanged(*/category/*) */);
+        try {
+            // TODO: Prevent deletion ...
+            categories.deleteAll();
+        } catch (Exception ex) {
+            throw new InternalServerError("An unexpected error has occurred. :(");
+        }
     }
 }
