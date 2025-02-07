9a462ae0031439b61a886d5268bf1f39f7c96dde
milicabosancic1 <milicabosancic1612@gmail.com>
2025-02-07 21:44:56 +0100

    fixed filter options for solutions

--- src/main/java/edu/ftn/iss/eventplanner/controllers/SolutionController.java
+++ src/main/java/edu/ftn/iss/eventplanner/controllers/SolutionController.java
@@ -6,9 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SolutionController {
