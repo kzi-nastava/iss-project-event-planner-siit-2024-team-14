ee4a4ea9955ce77df37b4940cace6e94a359e231
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2025-02-20 20:11:14 +0100

    Page response when returning a collection of services

--- src/main/java/edu/ftn/iss/eventplanner/controllers/ProviderServicesController.java
+++ src/main/java/edu/ftn/iss/eventplanner/controllers/ProviderServicesController.java
@@ -1,26 +1,24 @@
 package edu.ftn.iss.eventplanner.controllers;
 
 import edu.ftn.iss.eventplanner.dtos.CreateServiceDTO;
-import edu.ftn.iss.eventplanner.dtos.CreateServiceForProviderDTO;
 import edu.ftn.iss.eventplanner.dtos.ServiceDTO;
 import edu.ftn.iss.eventplanner.mappers.ServiceDTOMapper;
 import edu.ftn.iss.eventplanner.services.ServiceService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
 
 @RestController
-@RequestMapping(path = {"api/providers/{providerId:\\d+}/services", "api/services"})
+@RequestMapping(path = {"api/providers/{providerId:\\d+}/services"})
 public class ProviderServicesController {
 
     private final ServiceService serviceService;
@@ -33,18 +31,21 @@ public class ProviderServicesController {
     }
 
 
-
+    // GET */api/providers/1/services (Result differs across roles)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Collection<ServiceDTO>> getProviderServices( // TODO: if an admin or provider requesting his/hers services, return all, otherwise return only publicly visible services
-            @PathVariable(required = false, name = "providerId") Integer providerId
+    ResponseEntity<Page<ServiceDTO>> getProviderServices(
+            @PathVariable(name = "providerId") int providerId,
+            @RequestParam MultiValueMap<String, String> params,
+            Pageable pageable
     ) {
-        return ResponseEntity.ok(List.of());
+        params.keySet().removeAll(Set.of("page", "size", "sort"));
+        return ResponseEntity.ok(Page.empty());
     }
 
-
+    // POST provider[Is identified by id 1]|admin@*/api/providers/1/services
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<ServiceDTO> createService(
-            @PathVariable(required = true, name = "providerId") int providerId, // or just get it from token ??
+            @PathVariable(name = "providerId") int providerId, // or just get it from token ??
             @RequestBody @Validated CreateServiceDTO service
     ) {
         ServiceDTO createdService = /* TODO: implement */ new ServiceDTO();
--- src/main/java/edu/ftn/iss/eventplanner/controllers/ServiceController.java
+++ src/main/java/edu/ftn/iss/eventplanner/controllers/ServiceController.java
@@ -1,51 +1,57 @@
 package edu.ftn.iss.eventplanner.controllers;
 
-import edu.ftn.iss.eventplanner.dtos.CreateServiceForProviderDTO;
 import edu.ftn.iss.eventplanner.dtos.ServiceDTO;
 import edu.ftn.iss.eventplanner.dtos.UpdateServiceDTO;
+import edu.ftn.iss.eventplanner.entities.Service;
+import edu.ftn.iss.eventplanner.mappers.ServiceDTOMapper;
 import edu.ftn.iss.eventplanner.services.ServiceService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.Set;
+
 
 @RestController
 @RequestMapping(path = {"api/services"})
 public class ServiceController {
 
-    //private final ServiceService serviceService;
+    private final ServiceService serviceService;
+    private final ServiceDTOMapper modelMapper;
 
 
-    public ServiceController() {
-
+    public ServiceController(ServiceService serviceService, ServiceDTOMapper modelMapper) {
+        this.serviceService = serviceService;
+        this.modelMapper = modelMapper;
     }
 
 
-
+    // GET */api/services (Result differs across user roles)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Collection<ServiceDTO>> getAllServices() {
-        return ResponseEntity.ok(List.of());
-    }
-
-
-    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<ServiceDTO> getServiceById(
-            @PathVariable(name = "id") Long id
+    ResponseEntity<Page<ServiceDTO>> getAllServices(
+            @RequestParam MultiValueMap<String, String> params,
+            Pageable pageable
     ) {
-        return ResponseEntity.notFound().build();
+        params.keySet().removeAll(Set.of("page", "size", "sort"));
+        return ResponseEntity.ok(Page.empty());
+    }
+
+    // GET @*/api/services/1
+    @GetMapping(path = "/{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<ServiceDTO> getServiceById(
+            @PathVariable(name = "id") int id
+    ) {
+        Service service = serviceService.getServiceById(id); // maybe hide services that are not publicly visible
+        return ResponseEntity.ok(modelMapper.toServiceDTO(service));
     }
 
 
-
+    // PUT provider[Provides the service]|admin@*/api/services/1
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<ServiceDTO> putUpdateService(
             @RequestBody @Validated UpdateServiceDTO service
@@ -53,17 +59,17 @@ public class ServiceController {
         return ResponseEntity.accepted().build();
     }
 
-
+    // DELETE provider[Provides the service]|admin@*/api/services/1
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = "/{id:\\d+}")
     void deleteService(
-            @PathVariable(name = "id") long id
+            @PathVariable(name = "id") int id
     ) {
-
+        serviceService.deleteService(id);
     }
 
 
-
+    // DELETE admin@*/api/services
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @DeleteMapping
     void deleteAllServices() { }
