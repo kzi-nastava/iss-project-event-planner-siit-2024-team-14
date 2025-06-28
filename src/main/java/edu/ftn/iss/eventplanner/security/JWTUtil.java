48d301c2b941a92df4310ded329f089f45c69d05
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2025-06-28 18:22:18 +0200

    Add once per request filter to set authentication & handle `AuthenticationExceptions` nicely

--- src/main/java/edu/ftn/iss/eventplanner/security/JWTUtil.java
+++ src/main/java/edu/ftn/iss/eventplanner/security/JWTUtil.java
@@ -1,18 +1,20 @@
 package edu.ftn.iss.eventplanner.security;
 
 import edu.ftn.iss.eventplanner.entities.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.*;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 public class JWTUtil {
 
-    private static final String SECRET_KEY = "bXlzdHJvbmdzZWNyZXRrZXlmb3JldmVuY2UjfewnjJhjbhnVBDGHKJnjmebnjkwn";  // This is a Base64-encoded key
+    private static final String SECRET_KEY = "bXlzdHJvbmdzZWNyZXRrZXlmb3JldmVuY2UjfewnjJhjbhnVBDGHKJnjmebnjkwn",  // This is a Base64-encoded key
+        AUTH_HEADER = "Authorization";
 
     // Generate token
     public static String generateToken(User user) {
@@ -41,10 +43,15 @@ public class JWTUtil {
 
             Claims claims = parser.parseClaimsJws(token).getBody();
             return claims.getSubject(); // The subject is the email
-        } catch (Exception e) {
+        } catch (ExpiredJwtException e) {
+            throw e;
+        }
+        catch (Exception e) {
             throw new RuntimeException("Invalid token");
         }
     }
+
+    public static @Nullable String getUsername(String token) { return extractEmail(token); }
 
 
     // Validate if the token has expired
@@ -62,4 +69,15 @@ public class JWTUtil {
         Claims claims = parser.parseClaimsJws(token).getBody();
         return claims.getExpiration();
     }
+
+    @Nullable
+    public static String getToken(HttpServletRequest request) {
+        var auth = request.getHeader(AUTH_HEADER);
+        return auth != null && auth.startsWith("Bearer ") ? auth.substring(7) : null;
+    }
+
+    public static boolean isTokenValid(String token, UserDetails userDetails) {
+        return Objects.equals(getUsername(token), userDetails.getUsername()); // maybe check if the password has been changed after the token issuance
+    }
+
 }
