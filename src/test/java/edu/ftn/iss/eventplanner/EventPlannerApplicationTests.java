95d96de2e56292aacb56ef75a6363c2db5e46c5b
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2025-07-23 16:18:03 +0200

    Include budget planning integration tests

--- src/test/java/edu/ftn/iss/eventplanner/EventPlannerApplicationTests.java
+++ src/test/java/edu/ftn/iss/eventplanner/EventPlannerApplicationTests.java
@@ -1,13 +1,27 @@
 package edu.ftn.iss.eventplanner;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventPlannerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void usesH2Database(@Autowired DataSource dataSource) throws Exception {
+		String dbUrl = dataSource.getConnection().getMetaData().getURL();
+
+		assertTrue(dbUrl.contains("jdbc:h2"), "Expected H2 database, but got: " + dbUrl);
 	}
 
 }
